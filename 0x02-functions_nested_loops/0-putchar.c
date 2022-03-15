1b4c76c788621bb796ba879532b216861fbde7d0
Excelola <foheol2075@gmail.com>
2022-03-15 09:03:46 -0700

    Feat: update features

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar"
+
+/**
+ * main - print putchar, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
