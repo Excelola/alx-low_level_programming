58d9754f355a099d9c942eb3df09c63f09b20a60
Olaoluwa Folorunso <100338477+Excelola@users.noreply.github.com>
2022-03-15 15:42:49 -0700

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * add - add two integers
- * @a: two integer arguments
- * @b: two integer arguments
+ * add - adds two integers
+ * @n1: integer 1
+ * @n2: integer 2
+ *
+ * Description: give two integers, returns sum
+ *
  * Return: sum
  */
 
-int add(int a, int b)
+int add(int n1, int n2)
 {
-	return (a + b);
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
 }
