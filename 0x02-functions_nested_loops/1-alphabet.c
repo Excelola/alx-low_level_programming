48ad609530742cbff4e11f741423b3891de0ee43
Excelola <foheol2075@gmail.com>
2022-03-15 12:30:02 -0700

    Update file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * print_alphabet - check description
+ * Description: it prints the alphabet, in lowercase, followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
