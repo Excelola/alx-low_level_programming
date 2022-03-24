637266a61abe936f2e210281b543afc8f99271f5
Olaoluwa Folorunso <100338477+Excelola@users.noreply.github.com>
2022-03-24 04:34:42 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
-	
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
-	
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
-	return (dest);
-}
+char destination[] = "Hello ";
+    char source[] = "World!";
+    strcat(destination,source);
+    printf("Concatenated String: %s\n", destination);
+    return 0;
